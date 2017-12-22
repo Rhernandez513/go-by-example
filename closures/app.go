1c4e2bc9f48060c539f61d4eead1bcbc2781185e
Robert David Hernandez <rhernandez513@gmail.com>
2017-12-21 21:42:07 -0600

    closures, a language features i've never heard of before

---
+++ closures/app.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// closures retru na function that encapsulate an enviornment,
+// in this case closing-over i
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
+}
+
+func main() {
+
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
+}
