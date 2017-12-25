664d222e90e26a8856baca99d78e4454858d1e64
Robert David Hernandez <rhernandez513@gmail.com>
2017-12-25 12:18:06 -0600

    concurrency with goroutines

--- Goroutines/app.go
+++ Goroutines/app.go
@@ -1,1 +1,28 @@
-package Goroutines
+package main
+
+import "fmt"
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+	fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+	// synchronous execution
+	f("direct")
+
+	// concurrent execution
+	go f("goroutine")
+
+	// anonymous function call
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	// this line requires that we hit enter to complete program execution
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println("done")
+
+}
