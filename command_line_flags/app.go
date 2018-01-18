29028d68ee647d5ba8d2defefc3c70c3006a164d
Robert David Hernandez <rhernandez513@gmail.com>
2018-01-17 19:06:30 -0600

    command line flags

--- command_line_flags/app.go
+++ command_line_flags/app.go
@@ -1,1 +1,24 @@
-package command_line_flags
+package main
+
+import "flag"
+import "fmt"
+
+func main () {
+
+	// returns a string pointer and not a value
+	wordPtr := flag.String("word", "foo", "a string")
+
+	numbPtr := flag.Int("numb", 42, "an int")
+	boolPtr := flag.Bool("fork", false, "a bool")
+
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string var")
+
+	flag.Parse()
+
+	fmt.Println("word:", *wordPtr)
+	fmt.Println("numb", *numbPtr)
+	fmt.Println("fork", *boolPtr)
+	fmt.Println("svar", svar)
+	fmt.Println("tail", flag.Args())
+}
